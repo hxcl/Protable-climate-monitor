e1fcbc6cb3cd15c3ca62d1298d7176cdf566a415
ToddZhou <todd310378072@outlook.com>
2019-04-05 17:44:12 +0800

    stepper

--- stepper/stepper.ino
+++ stepper/stepper.ino
@@ -1,14 +1,33 @@
 #include<Arduino.h>
+#include <Wire.h>
+
 #define IN1 8
 #define IN2 9
 #define IN3 10
 #define IN4 11
+
+int clockwise=0;
+void rotate(int,int);
+
 void setup() 
 {
   pinMode(IN1,OUTPUT);
   pinMode(IN2,OUTPUT);
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
+  Serial.begin(9600);
+  Wire.begin(0x99);//括号内补充本机地址，作为从机
+  Wire.onReceive(receiveEvent);
+}
+
+void receiveEvent()
+{
+  while(Wire.available()>0)
+  {
+    clockwise=Wire.read();
+  }
+  Serial.println(clockwise);
+  rotate(100,clockwise);
 }
 
 void STEP(int i)
@@ -97,10 +116,10 @@ void rotate(int times, int clockwise=1)
   {
     if(clockwise==0)
     {
-      for(int i=0;0<8;++i)
+      for(int i=0;i<8;++i)
       {
         STEP(i);
-        delay(1);
+        delay(2);
       }
       STEP(8);
       delay(1);
@@ -110,7 +129,7 @@ void rotate(int times, int clockwise=1)
       for(int i=7;i>=0;--i)
       {
         STEP(i);
-        delay(1);
+        delay(2);
       }
       STEP(8);
       delay(1);
@@ -121,6 +140,6 @@ void rotate(int times, int clockwise=1)
 
 void loop() 
 {
-  rotate(100,0);
-  delay(1000);
+  delay(100);
+
 }
